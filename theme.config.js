a707320996a44f56aeafd9b67610edbe9cda680f
Angga Arkademi <angga.arkademi@gmail.com>
2022-06-10 14:55:33 +0700

    test change data

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Arkademi</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         The Next.js Static Site Generator
       </span>
